9ae3235f4137f9b3075c600852ece505e6784b7f
Layton Vincent-Stewart 10005924 <41661784+Guitarkid00@users.noreply.github.com>
2020-08-18 06:31:07 +1200

    Fixed battle system, start messing with scriptable objects
    Fixed the battle system (simple fix), started trying to make the units with scriptable object but was having trouble making them work with the in place battle system

---
+++ Assets/Scripts/ScriptableObject Practice/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    [SerializeField]
+    private UnitObject unitObject;
+
+    private void setValues()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/ScriptableObject Practice/UnitObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Unit", menuName = "Unit")]
+public class UnitObject : ScriptableObject
+{
+    //Atk short for Attack
+
+    public string unitName { get { return unitName; } }
+    public Sprite artwork { get { return artwork; } }
+    public int unitLevel { get { return unitLevel; } }
+    public int baseHP { get { return baseHP; } }
+    public int baseAtk { get { return baseAtk; } }
+    public int perLevelHP { get { return perLevelHP; } }
+    public int perLevelAtk { get { return perLevelAtk; } }
+    public int maxHP { get { return maxHP; } }
+    public int currentHP { get { return currentHP; } }
+    public int currentAtk { get { return currentAtk; } }
+}
