dcaa3e3a77c78898e175677c18c0ea00eaaa0d3d
Layton Vincent-Stewart 10005924 <41661784+Guitarkid00@users.noreply.github.com>
2020-08-31 10:59:16 +1200

    Set up basic UI and created most of the scenes
    Decided to stop working on battle system for now. Instead set up a splash screen for starting the game, and set up the basic UI in the UnitScene which will be used on most of the scenes for navigation

---
+++ Assets/Scripts/MainUIScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUIScript : MonoBehaviour
+{
+    public GameObject SettingsPanel;
+
+    public Slider MasterSlider;
+    public Slider MusicSlider;
+    public Slider ButtonSlider;
+
+    public void OpenSettings()
+    {
+        if(SettingsPanel != null)
+        {
+            SettingsPanel.SetActive(true);
+        }
+    }
+
+    public void CloseSettings()
+    {
+        SettingsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game has been CLOSED");
+        Application.Quit();
+    }
+
+    public void MuteAll() //Change this to mute mixer eventually instead of moving sliders
+    {
+        MasterSlider.value = -80;
+        MusicSlider.value = -80;
+        ButtonSlider.value = -80;
+    }
+}
