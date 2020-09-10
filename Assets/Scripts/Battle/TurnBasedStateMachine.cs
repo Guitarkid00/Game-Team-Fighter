685e60eec0d54434df716acef757f1e77470264e
Layton Vincent-Stewart 10005924 <41661784+Guitarkid00@users.noreply.github.com>
2020-09-11 08:35:04 +1200

    Renamed sprites, made some prefabs, reworking battle system
    Renamed all the sprites and started making the prefabs for each unit. Started work on a new BattleSystem script which I feel will suit what I'm doing better

---
+++ Assets/Scripts/Battle/TurnBasedStateMachine.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBasedStateMachine : MonoBehaviour
+{
+    public enum BattleStates
+    {
+        START,
+        PLAYERTURN,
+        ENEMYTURN,
+        CALCDAMAGE,
+        ADDSTATUSEFFECT,
+        WIN,
+        LOSE
+    }
+
+    private BattleStates currentState;
+
+    private void Start()
+    {
+        currentState = BattleStates.START;
+    }
+
+    private void Update()
+    {
+        Debug.Log(currentState);
+        switch (currentState)
+        {
+            case (BattleStates.START):
+                //Start State
+                break;
+            case (BattleStates.PLAYERTURN):
+                //Start State
+                break;
+            case (BattleStates.ENEMYTURN):
+                //Start State
+                break;
+            case (BattleStates.ADDSTATUSEFFECT):
+                //Start State
+                break;
+            case (BattleStates.CALCDAMAGE):
+                //Start State
+                break;
+            case (BattleStates.WIN):
+                //Start State
+                break;
+            case (BattleStates.LOSE):
+                //Start State
+                break;
+        }
+    }
+
+}
